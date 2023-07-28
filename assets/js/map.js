ee8e5c0649d239128dea374b67f86a3c5b50f53a
Kyle Jocoy <kjocoy@gmail.com>
2023-07-27 22:08:33 -0700

    added small features to Google Maps

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,16 +2,16 @@
 "use strict";
 
 function initMap() {
-  const myLatLng = {
-    lat: 40.12150192260742,
-    lng: -100.45039367675781
+  let myLatLng = {
+    lat: 38.5367299382404,
+    lng: -121.75132258093318
   };
-  const map = new google.maps.Map(document.getElementById("gmp-map"), {
-    zoom: 4,
+  let map = new google.maps.Map(document.getElementById("gmp-map"), {
+    zoom: 16,
     center: myLatLng,
     fullscreenControl: false,
     zoomControl: true,
-    streetViewControl: false
+    streetViewControl: true
   });
   new google.maps.Marker({
     position: myLatLng,
