d499e98f52204f1d24a6c310043051f89bff9ab7
Andrew Stave <andrewdstave@gmail.com>
2023-07-27 21:59:50 -0700

    started adding weather api into project. still needs testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,33 @@ document.getElementById("how-to-link").addEventListener("click", function (event
   targetElement.scrollIntoView({ behavior: "smooth" });
 });
 
+function fetchWeatherData(city, apiKey) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}units=imperial`;
+
+  // PUT IN WEATHER, TEMPERATURE, AND WIND SPEED  
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const windSpeedMeters = data.wind.speed;
+      const temperatureFahrenheit = data.main.temp;
+      const humidity = data.main.humidity;
+      const weatherConditions = data.weather[0].description;
+
+      console.log("Wind Speed:", windSpeedMeters);
+      console.log("Temperature:", temperatureFahrenheit);
+      console.log("Humidity:", humidity);
+      console.log("Weather Conditions:", weatherConditions);
+
+      document.getElementById("windSpeed").textContent = `Wind Speed: ${windSpeedMeters.toFixed(2)} m/s`;
+      document.getElementById("temperature").textContent = `Temperature: ${temperatureFahrenheit.toFixed(2)} F`;
+      document.getElementById("humidity").textContent = `Humidity: ${humidity}%`;
+      document.getElementById("weatherConditions").textContent = `Weather Conditions: ${weatherConditions}`;
+    })
+    .catch(error => {
+      console.error('Error fetching weather data:', error);
+    });
+}
+
 // Block of code copied from Google's maps platform
 let map;
 
@@ -26,5 +53,9 @@ async function initMap() {
     zoom: 8,
   });
 }
+
+const apiKey = 'de499e2ee729b13656959965fb76984a';
+const city = 'Sacramento';
+fetchWeatherData(city, apiKey);
 
 initMap();
